f483b2cc996af8590a557b99668b5471bc5a5c0f
Evan620 <evan620@example.com>
2025-08-27 07:10:56 +0300

    vite issues 2

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    // Suppress React DevTools warning in production
-    __REACT_DEVTOOLS_GLOBAL_HOOK__: mode === 'production' ? '{ isDisabled: true }' : 'undefined',
-  },
+
 }));
